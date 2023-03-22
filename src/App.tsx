cb088a85a01287bebd6e009e38a688e96d4a2e44
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-03-22 12:18:53 +0100

    reducer setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React ,{useReducer}from 'react';
 import './App.css';
 
+const intialState={
+  players:[
+    {id:1,name:'Cristiano Ronaldo',sport:'Soccer',country:'Portugal'},
+    {id:1,name:'LeBron James',sport:'Basketball',country:'United States'},
+    {id:1,name:'Lionel Messi',sport:'Soccer',country:'Argentina'},
+    {id:1,name:'Roger Federer',sport:'Tennis',country:'Switzerland'},
+    {id:1,name:'Rafael Nadal',sport:'Tennis',country:'Spain'}
+  ],
+  editId:0
+}
+
+interface IPlayer{
+  id:number,name:string,sport:string,country:string
+}
+interface IState{
+  players:IPlayer[],
+  editId:number
+}
+interface IAddPlayer{
+  name:string,sport:string,country:string
+}
+interface IAction{
+  type:string,
+  payload:IAddPlayer|number|IPlayer
+}
+function reducer(state:IState,action:IAction){
+switch(action.type){
+  // case "ADD_PLAYER":return {...state,players:[...state.players,action.payload]};
+  // case "DELETE_PLAYER": return {...state,players:state.players.filter((p)=>p.id!==action.payload)};
+  // case "EDIT_PLAYER":return {...state,players:state.players.map((p)=>p.id===state.editId?action.payload:p)};
+  default :return state;
+}
+}
 function App() {
+  const [state,dispatch]=useReducer(reducer,intialState);
   return (
-    <div>
-      <h1>React</h1>
+    <div>      
     </div>
   );
 }
